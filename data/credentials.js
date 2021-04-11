b38bdf552477cca5e8dfa5831c5b4c994edc0fbf
Shubham Dogra <43871644+ShubhamDogra112@users.noreply.github.com>
2021-04-11 13:45:47 +0530

    Update credentials.js

--- data/credentials.js
+++ data/credentials.js
@@ -1,6 +1,6 @@
 module.exports = {
-    email : 'dirtyclient62@gmail.com',
-    password: 'dirtytesting@62',
+    email : '',
+    password: '',
     'message':`Hi!, I am an aspiring developer looking to get into a company like yours. I have applied online and reached out to recruiters, but I'd love to hear any words of wisdom you might have as you've successfully landed at (company)! Since we have similar backgrounds, I was hoping to learn something from your success.
     Do you have any advice or tips you'd be willing to share? I would greatly appreciate it. If I can ever return the favor please let me know!
     Thanks`
